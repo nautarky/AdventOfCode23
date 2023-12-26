5b35774eb1169abf12876e4e737da5586a40c6c0
Matt D <1nautarky@gmail.com>
2023-12-25 19:26:41 -0800

    Finish D1P2

--- advent.go
+++ advent.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
-	input := shared.ReadFile("input/1_test.txt")
+	input := shared.ReadFile("input/1.txt")
 	fmt.Printf("D1P1: %d\n", day1.Part1(input))
+	fmt.Printf("D1P2: %d\n", day1.Part2(input))
 }
--- day1/day1.go
+++ day1/day1.go
@@ -1,6 +1,35 @@
 package day1
 
-import "Advent23/shared"
+import (
+	"Advent23/shared"
+	"strings"
+)
+
+var strIntMap = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+}
+
+type calibration struct {
+	value    int
+	position int
+}
 
 func Part1(lines []string) int {
 	sum := 0
@@ -22,6 +51,37 @@ func Part1(lines []string) int {
 		}
 
 		sum += first*10 + last
+	}
+
+	return sum
+}
+
+func Part2(lines []string) int {
+	sum := 0
+
+	for _, line := range lines {
+		first := calibration{position: len(line)}
+		last := calibration{position: -1}
+
+		for k, v := range strIntMap {
+			pos := strings.Index(line, k)
+
+			if pos == -1 {
+				continue
+			}
+
+			if pos < first.position {
+				first = calibration{v, pos}
+			}
+
+			pos = strings.LastIndex(line, k)
+
+			if pos > last.position {
+				last = calibration{v, pos}
+			}
+		}
+
+		sum += first.value*10 + last.value
 	}
 
 	return sum
