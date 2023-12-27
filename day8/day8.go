64e5f4623bddc29fc3bf90f529f84a72696ab649
Matt D <1nautarky@gmail.com>
2023-12-27 11:02:05 -0800

    Finish D8P2

--- day8/day8.go
+++ day8/day8.go
@@ -1,28 +1,28 @@
 package day8
 
-import "strings"
+import (
+	"Advent23/shared"
+	"strings"
+)
 
 func Part1(lines []string) int {
 	graph := buildGraph(lines[2:])
-	cur, steps := "AAA", 0
-
-	for cur != "ZZZ" {
-		instruction := lines[0][steps%len(lines[0])]
-
-		if instruction == 'L' {
-			cur = graph[cur][0]
-		} else {
-			cur = graph[cur][1]
-		}
-
-		steps++
-	}
-
-	return steps
+	cur := "AAA"
+	stop := func(s string) bool { return s == "ZZZ" }
+	return findCycleLen(graph, cur, lines[0], stop)
 }
 
 func Part2(lines []string) int {
-	return 0
+	graph := buildGraph(lines[2:])
+	positions := findStarts(graph)
+	cycles := make([]int, len(positions))
+	stop := func(s string) bool { return s[len(s)-1] == 'Z' }
+
+	for i, pos := range positions {
+		cycles[i] = findCycleLen(graph, pos, lines[0], stop)
+	}
+
+	return shared.LCM(cycles[0], cycles[1], cycles[2:]...)
 }
 
 func buildGraph(lines []string) map[string][2]string {
@@ -34,4 +34,36 @@ func buildGraph(lines []string) map[string][2]string {
 	}
 
 	return output
+}
+
+func findCycleLen(graph map[string][2]string, start string, instructions string, stop func(string) bool) int {
+	cur, steps := start, 0
+
+	for {
+		instruction := instructions[steps%len(instructions)]
+
+		var place int
+		if instruction == 'R' {
+			place = 1
+		}
+
+		cur = graph[cur][place]
+		steps++
+
+		if stop(cur) {
+			return steps
+		}
+	}
+}
+
+func findStarts(graph map[string][2]string) []string {
+	starts := make([]string, 0)
+
+	for k := range graph {
+		if k[len(k)-1] == 'A' {
+			starts = append(starts, k)
+		}
+	}
+
+	return starts
 }
