567e5a2d0d4c1a8a121d1ef59866c103ed457526
Matt D <1nautarky@gmail.com>
2023-12-27 20:53:01 -0800

    Finish D10P2

--- day10/day10.go
+++ day10/day10.go
@@ -1,8 +1,8 @@
 package day10
 
 import (
+	"Advent23/shared"
 	"errors"
-	"fmt"
 )
 
 func Part1(lines []string) int {
@@ -11,7 +11,8 @@ func Part1(lines []string) int {
 	paths, openings := g.findAdjacentPaths(start)
 	g.replaceStart(start, openings)
 
-	cur, prev := paths[0], start
+	cur := paths[0]
+	prev := start
 	steps := 1
 
 	for cur != start {
@@ -29,27 +30,31 @@ func Part1(lines []string) int {
 	return steps / 2
 }
 
-func debug(tiles [][]rune, cur point) {
-	t := tiles[cur.y][cur.x]
-
-	tiles[cur.y][cur.x] = '*'
-
-	fmt.Println("--------------------------------")
-	for _, row := range tiles {
-		fmt.Println(string(row))
-	}
-	fmt.Println("--------------------------------")
-
-	tiles[cur.y][cur.x] = t
-}
-
 func Part2(lines []string) int {
-	return 0
-}
+	g := newGrid(lines)
+	start, _ := g.findStart()
+	paths, openings := g.findAdjacentPaths(start)
+	g.replaceStart(start, openings)
 
-type point struct {
-	x int
-	y int
+	points := []shared.Point{start}
+	cur := paths[0]
+	prev := start
+
+	for cur != start {
+		paths = g.findPathsOut(cur)
+
+		if paths[0] == prev {
+			cur, prev = paths[1], cur
+		} else {
+			cur, prev = paths[0], cur
+		}
+
+		points = append(points, cur)
+	}
+
+	area := shared.ShoelaceTheorem(points)
+	picks := shared.PicksTheoremI(area, float64(len(points)))
+	return int(picks)
 }
 
 type grid struct {
@@ -66,38 +71,38 @@ func newGrid(lines []string) *grid {
 	return &grid{tiles}
 }
 
-func (g *grid) findStart() (point, error) {
+func (g *grid) findStart() (shared.Point, error) {
 	for i, row := range g.tiles {
 		for j, tile := range row {
 			if tile == 'S' {
-				return point{j, i}, nil
+				return shared.Point{X: j, Y: i}, nil
 			}
 		}
 	}
-	return point{}, errors.New("didn't find a start position")
+	return shared.Point{}, errors.New("didn't find a start position")
 }
 
-func (g *grid) findPathsOut(p point) []point {
-	switch g.tiles[p.y][p.x] {
+func (g *grid) findPathsOut(p shared.Point) []shared.Point {
+	switch g.tiles[p.Y][p.X] {
 	case '|':
-		return []point{{p.x, p.y - 1}, {p.x, p.y + 1}}
+		return []shared.Point{{p.X, p.Y - 1}, {p.X, p.Y + 1}}
 	case '-':
-		return []point{{p.x - 1, p.y}, {p.x + 1, p.y}}
+		return []shared.Point{{p.X - 1, p.Y}, {p.X + 1, p.Y}}
 	case 'L':
-		return []point{{p.x, p.y - 1}, {p.x + 1, p.y}}
+		return []shared.Point{{p.X, p.Y - 1}, {p.X + 1, p.Y}}
 	case 'J':
-		return []point{{p.x, p.y - 1}, {p.x - 1, p.y}}
+		return []shared.Point{{p.X, p.Y - 1}, {p.X - 1, p.Y}}
 	case '7':
-		return []point{{p.x - 1, p.y}, {p.x, p.y + 1}}
+		return []shared.Point{{p.X - 1, p.Y}, {p.X, p.Y + 1}}
 	case 'F':
-		return []point{{p.x + 1, p.y}, {p.x, p.y + 1}}
+		return []shared.Point{{p.X + 1, p.Y}, {p.X, p.Y + 1}}
 	}
 
-	return []point{}
+	return []shared.Point{}
 }
 
-func (g *grid) findAdjacentPaths(p point) ([]point, map[rune]bool) {
-	adjacent := make([]point, 0)
+func (g *grid) findAdjacentPaths(p shared.Point) ([]shared.Point, map[rune]bool) {
+	adjacent := make([]shared.Point, 0)
 	directions := map[rune]bool{
 		'n': false,
 		'e': false,
@@ -106,41 +111,41 @@ func (g *grid) findAdjacentPaths(p point) ([]point, map[rune]bool) {
 	}
 
 	// north
-	if p.y-1 > -1 {
-		t := g.tiles[p.y-1][p.x]
+	if p.Y-1 > -1 {
+		t := g.tiles[p.Y-1][p.X]
 
 		if t == '|' || t == '7' || t == 'F' {
-			adjacent = append(adjacent, point{p.x, p.y - 1})
+			adjacent = append(adjacent, shared.Point{X: p.X, Y: p.Y - 1})
 			directions['n'] = true
 		}
 	}
 
 	// east
-	if p.x+1 < len(g.tiles[p.y]) {
-		t := g.tiles[p.y][p.x+1]
+	if p.X+1 < len(g.tiles[p.Y]) {
+		t := g.tiles[p.Y][p.X+1]
 
 		if t == '-' || t == 'J' || t == '7' {
-			adjacent = append(adjacent, point{p.x + 1, p.y})
+			adjacent = append(adjacent, shared.Point{X: p.X + 1, Y: p.Y})
 			directions['e'] = true
 		}
 	}
 
 	// south
-	if p.y+1 < len(g.tiles) {
-		t := g.tiles[p.y+1][p.x]
+	if p.Y+1 < len(g.tiles) {
+		t := g.tiles[p.Y+1][p.X]
 
 		if t == '|' || t == 'J' || t == 'L' {
-			adjacent = append(adjacent, point{p.x, p.y + 1})
+			adjacent = append(adjacent, shared.Point{X: p.X, Y: p.Y + 1})
 			directions['s'] = true
 		}
 	}
 
 	// west
-	if p.x-1 > -1 {
-		t := g.tiles[p.y][p.x-1]
+	if p.X-1 > -1 {
+		t := g.tiles[p.Y][p.X-1]
 
 		if t == '-' || t == 'L' || t == 'F' {
-			adjacent = append(adjacent, point{p.x - 1, p.y})
+			adjacent = append(adjacent, shared.Point{X: p.X - 1, Y: p.Y})
 			directions['w'] = true
 		}
 	}
@@ -148,19 +153,19 @@ func (g *grid) findAdjacentPaths(p point) ([]point, map[rune]bool) {
 	return adjacent, directions
 }
 
-func (g *grid) replaceStart(start point, openings map[rune]bool) {
+func (g *grid) replaceStart(start shared.Point, openings map[rune]bool) {
 	switch {
 	case openings['n'] && openings['e']:
-		g.tiles[start.y][start.x] = 'L'
+		g.tiles[start.Y][start.X] = 'L'
 	case openings['n'] && openings['s']:
-		g.tiles[start.y][start.x] = '|'
+		g.tiles[start.Y][start.X] = '|'
 	case openings['n'] && openings['w']:
-		g.tiles[start.y][start.x] = 'J'
+		g.tiles[start.Y][start.X] = 'J'
 	case openings['s'] && openings['e']:
-		g.tiles[start.y][start.x] = 'F'
+		g.tiles[start.Y][start.X] = 'F'
 	case openings['s'] && openings['w']:
-		g.tiles[start.y][start.x] = '7'
+		g.tiles[start.Y][start.X] = '7'
 	case openings['w'] && openings['e']:
-		g.tiles[start.y][start.x] = '-'
+		g.tiles[start.Y][start.X] = '-'
 	}
 }
--- shared/math.go
+++ shared/math.go
@@ -26,3 +26,23 @@ func QuadraticFormula(a, b, c float64) (float64, float64) {
 	minus := (-b - math.Sqrt(b*b-4*a*c)) / (2 * a)
 	return plus, minus
 }
+
+type Point struct {
+	X int
+	Y int
+}
+
+func ShoelaceTheorem(points []Point) float64 {
+	sum := 0.0
+	for i := 0; i < len(points); i++ {
+		p1, p2 := points[i], points[(i+1)%len(points)]
+		sum += float64(p1.X*p2.Y) - float64(p1.Y*p2.X)
+	}
+
+	return math.Abs(sum / 2.0)
+}
+
+// PicksTheoremI solves for I
+func PicksTheoremI(a, b float64) float64 {
+	return -1 * (-a + (b / 2) - 1)
+}
