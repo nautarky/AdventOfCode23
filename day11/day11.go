f134558a223c1159d0053a120e49dbdd8a91ff5c
Matt D <1nautarky@gmail.com>
2023-12-28 20:42:28 -0800

    Finish D11P2

--- day11/day11.go
+++ day11/day11.go
@@ -2,28 +2,42 @@ package day11
 
 import (
 	"Advent23/shared"
-	"math"
 	"strings"
 )
 
 func Part1(lines []string) int {
+	return solve(lines, 1)
+}
+
+func Part2(lines []string) int {
+	return solve(lines, 999999)
+}
+
+func solve(lines []string, offset int) int {
 	cols := findExpandingCols(lines)
 	rows := findExpandingRows(lines)
-	points := findPoints(lines, cols, rows)
+	points := findPoints(lines, cols, rows, offset)
 
 	sum := 0
 
 	for i, p1 := range points[:len(points)-1] {
 		for _, p2 := range points[i+1:] {
-			sum += int(math.Abs(float64(p1.X-p2.X)) + math.Abs(float64(p1.Y-p2.Y)))
+			x := p1.X - p2.X
+			if x < 0 {
+				x *= -1
+			}
+
+			y := p1.Y - p2.Y
+			if y < 0 {
+				y *= -1
+			}
+
+			sum += x
+			sum += y
 		}
 	}
 
 	return sum
-}
-
-func Part2(lines []string) int {
-	return 0
 }
 
 func findExpandingRows(lines []string) map[int]bool {
@@ -57,7 +71,7 @@ func findExpandingCols(lines []string) map[int]bool {
 	return cols
 }
 
-func findPoints(lines []string, bigCols map[int]bool, bigRows map[int]bool) []shared.Point {
+func findPoints(lines []string, bigCols map[int]bool, bigRows map[int]bool, offset int) []shared.Point {
 	points := make([]shared.Point, 0)
 
 	for y, grownY := 0, 0; y < len(lines); y++ {
@@ -73,7 +87,7 @@ func findPoints(lines []string, bigCols map[int]bool, bigRows map[int]bool) []sh
 			}
 
 			if lines[y][x] == '#' {
-				points = append(points, shared.Point{X: x + grownX, Y: y + grownY})
+				points = append(points, shared.Point{X: x + grownX*offset, Y: y + grownY*offset})
 			}
 		}
 	}
