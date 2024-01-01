37e1a97848fca38e543225786cb6f2b22b7a157a
Matt D <1nautarky@gmail.com>
2023-12-31 19:25:01 -0800

    Finish D15P2

--- day15/day15.go
+++ day15/day15.go
@@ -1,21 +1,67 @@
 package day15
 
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
 func Part1(lines []string) int {
-	sum, cur := 0, 0
-
-	for _, b := range lines[0] {
-		if b == ',' {
-			sum += cur
-			cur = 0
-			continue
-		}
-
-		cur = ((cur + int(b)) * 17) % 256
+	sum := 0
+	for _, s := range strings.Split(lines[0], ",") {
+		sum += hash(s)
 	}
-
-	return sum + cur
+	return sum
 }
 
 func Part2(lines []string) int {
-	return 0
+	boxes := make([]box, 256)
+	parts := strings.Split(lines[0], ",")
+
+	for _, p := range parts {
+		opPos := strings.IndexAny(p, "=-")
+		op := p[opPos]
+
+		label := p[:opPos]
+		b := hash(label)
+
+		if op == '=' {
+			fl, _ := strconv.Atoi(p[opPos+1:])
+			s := lens{label, fl}
+
+			i := slices.IndexFunc(boxes[b], func(l lens) bool { return l.label == label })
+			if i == -1 {
+				boxes[b] = append(boxes[b], s)
+			} else {
+				boxes[b][i] = s
+			}
+		} else {
+			boxes[b] = slices.DeleteFunc(boxes[b], func(l lens) bool { return l.label == label })
+		}
+	}
+
+	sum := 0
+
+	for bi, b := range boxes {
+		for li, l := range b {
+			sum += (bi + 1) * (li + 1) * l.focal
+		}
+	}
+
+	return sum
+}
+
+type box []lens
+
+type lens struct {
+	label string
+	focal int
+}
+
+func hash(s string) int {
+	cur := 0
+	for _, b := range s {
+		cur = ((cur + int(b)) * 17) % 256
+	}
+	return cur
 }
