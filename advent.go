b725119201c2ad4497971b8f2c62a692de45f77f
Matt D <1nautarky@gmail.com>
2024-01-03 21:21:38 -0800

    D17 prep

--- advent.go
+++ advent.go
@@ -4,11 +4,10 @@ import (
 	"Advent23/day1"
 	"Advent23/day10"
 	"Advent23/day11"
-	"Advent23/day12"
 	"Advent23/day13"
 	"Advent23/day14"
 	"Advent23/day15"
-	"Advent23/day16"
+	"Advent23/day17"
 	"Advent23/day2"
 	"Advent23/day3"
 	"Advent23/day4"
@@ -33,11 +32,12 @@ func main() {
 	printOutput("input/9.txt", day9.Part1, day9.Part2, 9)
 	printOutput("input/10.txt", day10.Part1, day10.Part2, 10)
 	printOutput("input/11.txt", day11.Part1, day11.Part2, 11)
-	printOutput("input/12.txt", day12.Part1, day12.Part2, 12)
+	// printOutput("input/12.txt", day12.Part1, day12.Part2, 12)
 	printOutput("input/13.txt", day13.Part1, day13.Part2, 13)
 	printOutput("input/14.txt", day14.Part1, day14.Part2, 14)
 	printOutput("input/15.txt", day15.Part1, day15.Part2, 15)
-	printOutput("input/16.txt", day16.Part1, day16.Part2, 16)
+	// printOutput("input/16.txt", day16.Part1, day16.Part2, 16)
+	printOutput("input/17test.txt", day17.Part1, day17.Part2, 17)
 }
 
 type solution func([]string) int
---
+++ day17/day17.go
@@ -0,0 +1,9 @@
+package day17
+
+func Part1(lines []string) int {
+	return 0
+}
+
+func Part2(lines []string) int {
+	return 0
+}
