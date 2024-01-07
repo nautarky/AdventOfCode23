545e6ccb7d9241c5576110f6d698510f17801914
Matt D <1nautarky@gmail.com>
2024-01-07 01:11:45 -0800

    Add 0's

--- advent.go
+++ advent.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"Advent23/day1"
+	"Advent23/day01"
+	"Advent23/day02"
+	"Advent23/day03"
+	"Advent23/day04"
+	"Advent23/day05"
+	"Advent23/day06"
+	"Advent23/day07"
+	"Advent23/day08"
+	"Advent23/day09"
 	"Advent23/day10"
 	"Advent23/day11"
 	"Advent23/day13"
 	"Advent23/day14"
 	"Advent23/day15"
-	"Advent23/day2"
-	"Advent23/day3"
-	"Advent23/day4"
-	"Advent23/day5"
-	"Advent23/day6"
-	"Advent23/day7"
-	"Advent23/day8"
-	"Advent23/day9"
 	"Advent23/shared"
 	"fmt"
 )
 
 func main() {
-	printOutput("input/1.txt", day1.Part1, day1.Part2, 1)
-	printOutput("input/2.txt", day2.Part1, day2.Part2, 2)
-	printOutput("input/3.txt", day3.Part1, day3.Part2, 3)
-	printOutput("input/4.txt", day4.Part1, day4.Part2, 4)
-	printOutput("input/5.txt", day5.Part1, day5.Part2, 5)
-	printOutput("input/6.txt", day6.Part1, day6.Part2, 6)
-	printOutput("input/7.txt", day7.Part1, day7.Part2, 7)
-	printOutput("input/8.txt", day8.Part1, day8.Part2, 8)
-	printOutput("input/9.txt", day9.Part1, day9.Part2, 9)
+	printOutput("input/1.txt", day01.Part1, day01.Part2, 1)
+	printOutput("input/2.txt", day02.Part1, day02.Part2, 2)
+	printOutput("input/3.txt", day03.Part1, day03.Part2, 3)
+	printOutput("input/4.txt", day04.Part1, day04.Part2, 4)
+	printOutput("input/5.txt", day05.Part1, day05.Part2, 5)
+	printOutput("input/6.txt", day06.Part1, day06.Part2, 6)
+	printOutput("input/7.txt", day07.Part1, day07.Part2, 7)
+	printOutput("input/8.txt", day08.Part1, day08.Part2, 8)
+	printOutput("input/9.txt", day09.Part1, day09.Part2, 9)
 	printOutput("input/10.txt", day10.Part1, day10.Part2, 10)
 	printOutput("input/11.txt", day11.Part1, day11.Part2, 11)
 	// printOutput("input/12.txt", day12.Part1, day12.Part2, 12)
